b930023aba48a1c6dbef2f0eddf97e9be53a22fe
Ender <33337309+e3ndr@users.noreply.github.com>
2021-12-05 20:37:23 -0600

    Adjust the art for windows, add support for < windows8 and windows11

--- src/main/java/xyz/e3ndr/jeofetch/config/OperatingSystems.java
+++ src/main/java/xyz/e3ndr/jeofetch/config/OperatingSystems.java
@@ -7,15 +7,30 @@ public class OperatingSystems {
 
     // These all are to match the properties via regex.
     private static final SystemInfo[] operatingSystems = {
-            new SystemInfo("windows.txtart")
-                .withOs("^Windows"),
-            new SystemInfo("apple.txtart")
-                .withOs("(Darwin|mac)"),
+
+            // Microsoft
+            new SystemInfo("windows-square.txtart")
+                .withOs("Windows 11"),
+
+            new SystemInfo("windows-metro.txtart")
+                .withOs("Windows")
+                .withKernel("(6\\.2|6\\.3||10\\.0)"),
+
+            new SystemInfo("windows-flag.txtart")
+                .withOs("Windows"),
+
             new SystemInfo("wsl.txtart")
                 .withOs("^Linux")
                 .withKernel("Microsoft"),
+
+            // Apple
+            new SystemInfo("apple.txtart")
+                .withOs("(Darwin|mac|Mac)"),
+
+            // Linux
             new SystemInfo("tux.txtart")
                 .withOs("^Linux"),
+
     };
 
     public static AsciiArt getArtForSystem(String force) {
