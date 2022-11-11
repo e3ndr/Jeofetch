070aa81c5955603074d56c1f2531e3712f9d691b
Ender <33337309+e3ndr@users.noreply.github.com>
2022-11-11 13:39:54 -0600

    Add a debug printer.

--- src/main/java/xyz/e3ndr/jeofetch/Bootstrap.java
+++ src/main/java/xyz/e3ndr/jeofetch/Bootstrap.java
@@ -1,9 +1,14 @@
 package xyz.e3ndr.jeofetch;
 
+import java.io.IOException;
+
+import co.casterlabs.commons.platform.Platform;
 import lombok.Getter;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import xyz.e3ndr.jeofetch.platform.PlatformUtils;
+import xyz.e3ndr.jeofetch.platform.handler.PlatformHandler;
 
 @Getter
 @Command(name = "print", mixinStandardHelpOptions = true, version = ":^)", description = "Prints a neofetch-esque report")
@@ -39,6 +44,12 @@ public class Bootstrap implements Runnable {
     }, description = "Forces a piece of art to be on screen")
     private String forced = null;
 
+    @Option(names = {
+            "-d",
+            "--debug"
+    }, description = "Prints debug system info and exits")
+    private boolean debug = false;
+
     public static void main(String[] args) {
 //        try {
 //            ConsoleUtil.summonConsoleWindow();
@@ -55,6 +66,22 @@ public class Bootstrap implements Runnable {
 //                ConsoleUtil.setTitle("Jeofetch");
 //            } catch (IOException | InterruptedException e) {}
 //        }
+
+        if (this.debug) {
+            PlatformHandler handler = PlatformUtils.getSystemHandler();
+
+            System.out.printf("Java os.name:             %s\n", System.getProperty("os.name"));
+            System.out.printf("Platform Arch:            %s\n", Platform.arch);
+            System.out.printf("Platform OS Family:       %s\n", Platform.osFamily);
+            System.out.printf("Platform OS Distribution: %s\n", Platform.osDistribution);
+            System.out.printf("Platform Handler:         %s\n", handler.getClass().getCanonicalName());
+            try {
+                System.out.printf("Kernel:                   %s\n", handler.getKernel());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
 
         try {
             Jeofetch.print(this);
